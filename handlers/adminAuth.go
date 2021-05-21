d39fe54acafc6cd310fd7f28c3a5d68c14651542
shabry-nozyra <shabry@nozyra.com>
2021-05-21 09:54:31 +0700

    update last cookies

--- handlers/adminAuth.go
+++ handlers/adminAuth.go
@@ -216,7 +216,7 @@ func (ctx *Context) Loginadmin(c *gin.Context){
 		return
 	}
 
-	c.SetCookie("jwt", token, 60*60*24, "/", "https://pantaufront.azurewebsites.net/", true, false)
+	c.SetCookie("jwt", token, 60*60*24, "/", "pantaufront.azurewebsites.net", true, false)
 
 	c.JSON(http.StatusOK, "success")
 }
